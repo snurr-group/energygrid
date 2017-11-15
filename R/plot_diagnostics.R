f95d7adde3b203f3ab267023cd0fe04762745ec2
Ben Bucior <bbucior@u.northwestern.edu>
2017-11-15 09:55:41 -0600

    List important plots they can be implemented reusably

---
+++ R/plot_diagnostics.R
@@ -0,0 +1,51 @@
+# Makes diagnostic plots, such as model fit or distributions of bins
+
+library(ggplot2)
+library(dplyr)
+
+
+parity_line <- geom_abline(slope=1, intercept=0, linetype="dashed")
+
+parity_plot <- function(act, pred, color=1, alpha=0.05) {
+  # Parity plot between actual and predicted data, on square axes for g/L H2
+  qplot(act, pred, alpha=I(alpha), color=I(color)) +
+    xlab("'Actual' uptake (GCMC simulations)") +
+    ylab("Predicted uptake (ridge regression)") +
+    expand_limits(x = 0, y = 0) +
+    scale_x_continuous(limits = c(0,50)) +
+    scale_y_continuous(limits = c(0,50)) +
+    parity_line
+}
+
+
+plot_bin_z_vs_y <- function(zs, y, betas) {
+  # Plots the 2D distribution of z-score and y within each histogram bin, colored by model beta coef
+  mod_betas <- betas %>% mutate(rbeta = round(beta, 2))
+  
+  bind_cols(z=zs, y=y) %>% 
+    gather(key="bin", value="qty", -y) %>% 
+    left_join(mod_betas, by="bin") %>%
+    ggplot(aes(qty, y, col=rbeta)) +
+    geom_point() +
+    facet_wrap(~bin, scales="free_x") +
+    xlab("z-score in bin") +
+    theme(
+      text = element_text(size=8),
+      aspect.ratio = 0.75
+    ) +
+    scale_color_gradientn(colors = c("red", "darkgray", "blue"),
+                          #guide=guide_colorbar(title=expression(beta)))
+                          guide=guide_colorbar(title="beta"))
+}
+# Example: # plot_bin_z_vs_y(p_test_mod$x, p_test_mod$y, coef_tbl(p_test_mod$mod))
+
+# run_model_on_partitions
+
+
+# More functions:
+# Code that runs the model on training and test data partitions (for loop with ridge and LASSO, with the corresponding plots)
+## Maybe make that loop return an even larger data structure/class with a print.classname type helper function?
+## run_model_on_partitions
+
+# Other plots in old notebooks:
+# Q2 vs. bin settings (short code and doesn't need to be generalized)
--- R/plot_hists.R
+++ R/plot_hists.R
@@ -3,41 +3,11 @@
 library(ggplot2)
 library(dplyr)
 
-parity_line <- geom_abline(slope=1, intercept=0, linetype="dashed")
-
-parity_plot <- function(act, pred, color=1, alpha=0.05) {
-  # Parity plot between actual and predicted data, on square axes for g/L H2
-  qplot(act, pred, alpha=I(alpha), color=I(color)) +
-    xlab("'Actual' uptake (GCMC simulations)") +
-    ylab("Predicted uptake (ridge regression)") +
-    expand_limits(x = 0, y = 0) +
-    scale_x_continuous(limits = c(0,50)) +
-    scale_y_continuous(limits = c(0,50)) +
-    parity_line
-}
-
-plot_bin_z_vs_y <- function(zs, y, betas) {
-  # Plots the 2D distribution of z-score and y within each histogram bin, colored by model beta coef
-  mod_betas <- betas %>% mutate(rbeta = round(beta, 2))
-  
-  bind_cols(z=zs, y=y) %>% 
-    gather(key="bin", value="qty", -y) %>% 
-    left_join(mod_betas, by="bin") %>%
-    ggplot(aes(qty, y, col=rbeta)) +
-    geom_point() +
-    facet_wrap(~bin, scales="free_x") +
-    xlab("z-score in bin") +
-    theme(
-      text = element_text(size=8),
-      aspect.ratio = 0.75
-    ) +
-    scale_color_gradientn(colors = c("red", "darkgray", "blue"),
-                          #guide=guide_colorbar(title=expression(beta)))
-                          guide=guide_colorbar(title="beta"))
-}
-# Example: # plot_bin_z_vs_y(p_test_mod$x, p_test_mod$y, coef_tbl(p_test_mod$mod))
-
 
 # More functions:
-# beta plot (with adjustable (automatic?) histogram bins)
-# histogram density
+# beta plot (with adjustable (automatic?) histogram bins): this is the big one (and most important)
+## Also for multiple betas or multiple pressures: generalize the first one
+# histogram density: merge content from partial_plot_histogram_for_hists.R (figures in model explanation)
+
+# Other plots in old notebooks:
+# Heatmap randomization cartoon
