627f53fae9f92bb073aff9fd5773e9b960af7896
Ben Bucior <bbucior@u.northwestern.edu>
2017-11-15 08:37:47 -0600

    Highlight ToBaCCo predictions with hMOF topologies
    Searched for accuracy based on structural similarity.

--- R/plot_hists.R
+++ R/plot_hists.R
@@ -5,8 +5,8 @@ library(dplyr)
 
 parity_line <- geom_abline(slope=1, intercept=0, linetype="dashed")
 
-parity_plot <- function(act, pred, color=1) {
-  qplot(act, pred, alpha=I(0.05), color=I(color)) +
+parity_plot <- function(act, pred, color=1, alpha=0.05) {
+  qplot(act, pred, alpha=I(alpha), color=I(color)) +
     xlab("'Actual' uptake (GCMC simulations)") +
     ylab("Predicted uptake (ridge regression)") +
     expand_limits(x = 0, y = 0) +
