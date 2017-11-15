aadc460c2e6ae97c05bb32e18a5ef12c075f6706
Ben Bucior <bbucior@u.northwestern.edu>
2017-11-15 13:23:32 -0600

    Generalize beta/hist plot for number of categories
    If `cat` column is specified in the beta tibble, display categorical
    data via ggplot2.  Else, scale the color of the points according to
    their height.

--- R/plot_hists.R
+++ R/plot_hists.R
@@ -6,7 +6,7 @@ library(dplyr)
 # e.g. plot_hist_bins(filter(hist_vals, id==55), default_binspec)
 plot_hist_bins <- function(one_grid, binspec) {
   # Returns an energy histogram plot, possibly as a base layer to beta coefficients
-  # TODO: add in bin colors, which will require calculating them first
+  # TODO: add in colors for the histogram bin fill, which will require calculating them first
   one_grid %>% 
     stepped_hist_spec(binspec) %>% 
     mutate(height = metric) %>%
@@ -15,7 +15,7 @@ plot_hist_bins <- function(one_grid, binspec) {
     # left_join(trained_betas, by="bin") %>% 
     ggplot(aes(loc, height)) +
     geom_col() +
-    #geom_col(fill=rgb(HIST_COLORS_JUNK)) +  # THIS IS THE BIGGEST ONE
+    #geom_col(fill=rgb(HIST_COLORS_JUNK)) +  # THIS WILL REQUIRE QUITE SOME EFFORT TO GENERALIZE
     labs(
       x = "Energy (kJ/mol)",
       y = NULL
@@ -25,30 +25,31 @@ plot_hist_bins <- function(one_grid, binspec) {
     scale_y_continuous(
       labels = NULL,
       breaks = NULL
-    )
+    ) +
+    scale_x_continuous(minor_breaks = NULL)
 }
 # plot_hist_bins(filter(hist_sets$training, id==1), default_binspec) + coord_cartesian(ylim=c(-0.6, 0.6))
 
-# Add in betas as an additive plot layer?  TODO
-overlay_color_betas <- function(hist_plot, betas, binspec) {
-  # Function that takes a histogram plot and overlays the model betas in a single color
-  # TODO: stub
-  hist_plot
-}
-
 overlay_cat_betas <- function(hist_plot, betas, binspec, scaling = 10.0, hist_max = 0.5) {
   # Overlay betas from one or more categories, saved in the column `cat`, on top of a histogram plot
+  # If `cat` is not defined, use a colorbar based on the magnitude of beta.
+  
+  cat_missing <- FALSE  # Category not specified.  What a sad variable name  :(
+  
   beta_data <- betas %>%
     inner_join(bin_loc_from_spec(binspec), by="bin") %>% 
     mutate(beta = beta / scaling)
-  if (!("cat" %in% colnames(beta_data))) {
-    beta_data <- beta_data %>% mutate(`cat` = "1")
+  if ("cat" %in% colnames(beta_data)) {
+    beta_data <- beta_data %>% mutate(color = `cat`)
+  } else {
+    cat_missing <- TRUE
+    beta_data <- beta_data %>% mutate(color = beta)
   }
   
-  hist_plot +
+  p <- hist_plot +
     geom_point(
       data = beta_data,
-      aes(y = beta, col = cat),
+      aes(y = beta, col = color),
       size = 3
       ) +
     coord_cartesian(ylim = c(-1.1*hist_max, 1.1*hist_max)) +
@@ -59,11 +60,20 @@ overlay_cat_betas <- function(hist_plot, betas, binspec, scaling = 10.0, hist_ma
       sec.axis = sec_axis(
         ~.*scaling,
         name = expression(beta),
-        breaks = c(-1*hist_max*scaling, hist_max*scaling)
+        breaks = c(-1*hist_max*scaling, 0, hist_max*scaling)
         )
       ) +
     theme(axis.title.y.right = element_text(angle=0, vjust = 0.5)) +
     geom_hline(yintercept=0)
+  
+  if (cat_missing) {  # use colorbar
+    p <- p +
+      scale_color_gradientn(colors = c("red", "darkgray", "blue"), guide = "none") +
+      theme(axis.text.y.right = element_text(color = c("red", "darkgray", "blue")))
+  } else {
+    p <- p + theme(legend.title = element_blank())
+  }
+  p
 }
 
 # More functions:
