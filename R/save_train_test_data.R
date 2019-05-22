82cafedd4aff1b18cd5261d26774d9ca1abcd81c
zhaoli2023 <43864970+Zhaoli2042@users.noreply.github.com>
2019-05-22 16:26:15 -0500

    convert the original export_test_data function to a general function. Since it requires gcmc_data data frame, we can use it to add more columns to the csv file i.e. heat of adsorption or others...

--- R/save_train_test_data.R
+++ R/save_train_test_data.R
@@ -1,14 +1,16 @@
 library(magrittr)
 # save all the data to a csv file
-export_training_data <- function(partitioned_mod, filename) {
-  # %$% exposition pipe operator, magrittr package
-  partitioned_mod$trained_mod %$% bind_cols(y=y, orig_x) %>% write_csv(filename)
-  
-}
+# export_training_data <- function(partitioned_mod, filename) {
+#   # %$% exposition pipe operator, magrittr package
+#   partitioned_mod$trained_mod %$% bind_cols(y=y, orig_x) %>% write_csv(filename)
+#   
+# }
 
 
 # Now get the testing data, using a combination of pred_grid/eval_test_grid.
-export_testing_data <- function(test_grid, df_with_y_act, filename, binspec, align_bins="strict") {
+# convert it to a general function, not just for test data
+# can combine with other data like heat of adsorption
+export_data <- function(test_grid, df_with_y_act, filename, binspec, align_bins="strict") {
   grid_desc <- test_grid %>%
     stepped_hist_spec(binspec, align_bins = align_bins) %>% 
     spread(key=bin, value=metric)
