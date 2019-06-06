aa23324f97bc7279137c928f9dbe3fe6c340544d
zhaoli2023 <43864970+Zhaoli2042@users.noreply.github.com>
2019-06-06 15:46:07 -0500

    Added a function that reads textural properties and combine them with the training/testing data like energygrids, qst and etc.

--- R/save_train_test_data.R
+++ R/save_train_test_data.R
@@ -15,8 +15,24 @@ export_data <- function(test_grid, df_with_y_act, filename, binspec, align_bins=
     stepped_hist_spec(binspec, align_bins = align_bins) %>% 
     spread(key=bin, value=metric)
   results <- df_with_y_act %>% 
-    select(id, y_act) %>% 
+#    select(id, y_act) %>%
+    select(id, y_act, Heat_of_Ads,`Host-Guest`,`Guest-Guest`,Qst_low_loading) %>% 
     inner_join(grid_desc, by="id") %>% 
     select(-id)
+  results %>% write_csv(filename)
+}
+# extra function for extracting a separate set of topology data from other training purposes
+# Just toplogies
+export_topology_data <- function(test_grid, df_with_y_act,topo_data,filename){
+  # first get those IDs
+  new_df <- subset(df_with_y_act, df_with_y_act$id %in% test_grid$id)
+  # join it with gcmc_data
+  topo_data$MOF.ID <- as.character(topo_data$MOF.ID)
+  colnames(topo_data)[colnames(topo_data) == "MOF.ID"] <- "id"
+  
+  results <- topo_data %>% 
+  select(id,vf,vsa,gsa,pld,lcd) %>% 
+  inner_join(new_df, by = "id") %>% 
+  select(`Guest-Guest`,vf,vsa,gsa,pld,lcd)
   results %>% write_csv(filename)
 }
