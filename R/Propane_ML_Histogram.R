421433a29e05f501d2b55ffbc02c68687604ad4e
zhaoli2023 <43864970+Zhaoli2042@users.noreply.github.com>
2019-07-23 17:39:26 -0500

    Cleaned up some unused code Now the code runs the LASSO and randomforest models and generates plots and saves them. It is more like a package now.
    Will clean up the topology related stuff later.

--- R/Propane_ML_Histogram.R
+++ R/Propane_ML_Histogram.R
@@ -1,85 +1,75 @@
 rm(list=ls())
 
-
+library(plotly)
+library(randomForest)
+library(cowplot)
 source("R/plot_hists.R")
 source("R/regression_and_features.R")
 source("R/get_energy_stats.R")
 source("R/plot_diagnostics.R")
-library(plotly)
-library(randomForest)
 source("R/read_gcmc_data.R")
 source("R/tobacco_data_for_zhao.R")
 source("R/save_train_test_data.R")
 source("R/refined_bins_calc.R")
 # in command prompt: Rscript --vanilla R\save_h2_hists.R whatever.rds Energies\ use_ch4
-
- #ch4_binspec <- c(from=-26 , to=20, step=0.5, width=0.5)
- 
- hmof_h2_grid <- read_rds("All_data/CH3_probe_1A_UFF.rds")
- gcmc_data <- read_data("All_data/propane_1bar_UFF_2000points.txt")
+ # define the input files
+ grid_file = "All_data/CH3_probe_1A_UFF.rds"
+ gcmc_file = "All_data/ethane_10bar_UFF.txt"
+ if (!dir.exists("Results")){
+   dir.create("Results")
+ }
+ hmof_h2_grid <- read_rds(grid_file)
+ gcmc_data <- read_data(gcmc_file)
+ # extract the molecule name for picture naming
+ molecule_name <- sub(".*\\/", "", gcmc_file) %>% sub("\\_.*", "", .)
  # here we can have two options, feed uniform or non-uniform bins
  #binbounds <- bounds_from_params(ch4_binspec)
  gcmc_data <- mutate(gcmc_data, id=ID)
  new_grid <- hmof_h2_grid[hmof_h2_grid[, "id"] %in% gcmc_data$id,] # select out the grids with id same as gcmc_data id
- binbounds <- automatic_bins(new_grid,1000000) # for non-uniform bins
- ch4_binspec <- list("from" = -40, "to" = 40, bounds = binbounds)
+ binbounds <- automatic_bins(new_grid) # for non-uniform bins
+ ch4_binspec <- list("from" = head(binbounds$lower, n = 1), "to" = tail(binbounds$upper, n = 1), bounds = binbounds)
 
- DATA_SPLIT <- 1000 # Number of data points used in training data, from setup_data.R
+ 
+ # depending on the size of the dataset, data split is going to have two options
+ if (nrow(gcmc_data) >= 2000){
+   DATA_SPLIT <- 1000 # Number of data points used in training data, from setup_data.R
+ } else {
+   DATA_SPLIT <- ceiling(0.5 * (nrow(gcmc_data)))
+ }
+ 
 p_ch4_sets <- partition_data_subsets(hmof_h2_grid, gcmc_data, DATA_SPLIT)
 p_ch4_vol <- gcmc_data %>%
   mutate(g.L = Molec_cm3overcm3) %>%
   run_model_on_partitions(p_ch4_sets, ., ch4_binspec, plot_units="cm\u00B3/cm\u00B3", db_name = "tobacco")
 
-p_ch4_vol$plots$parity_testing%>% rescale_ch4_parity() + theme(axis.title.y = element_text(hjust=0.5)) + xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)")
+gg_train <- p_ch4_vol$plots$parity_training%>% rescale_ch4_parity() + theme(axis.title.y = element_text(hjust=0.5)) + xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)")
+gg_test <- p_ch4_vol$plots$parity_testing%>% rescale_ch4_parity() + theme(axis.title.y = element_text(hjust=0.5)) + xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)")
+save_plot(paste("Results/", paste0(molecule_name, "_train.png"), sep = ""),gg_train, base_width = 10, base_height = 8, dpi = 600)
+save_plot(paste("Results/", paste0(molecule_name, "_test.png"), sep = ""),gg_test, base_width = 10, base_height = 8, dpi = 600)
+# export data for later testing other models, one can also export data to csv files
+ train_data <- export_data(p_ch4_sets$training, rename(gcmc_data %>% mutate(g.L = Molec_cm3overcm3), y_act=g.L), ch4_binspec)
+ test_data <- export_data(p_ch4_sets$testing, rename(gcmc_data %>% mutate(g.L = Molec_cm3overcm3), y_act=g.L), ch4_binspec)
 
-# # save data to csv files for later testing other models
- export_data(p_ch4_sets$training, rename(gcmc_data %>% mutate(g.L = Molec_cm3overcm3), y_act=g.L), "tobacco_c3_1bar_train_28K.csv", ch4_binspec)
- export_data(p_ch4_sets$testing, rename(gcmc_data %>% mutate(g.L = Molec_cm3overcm3), y_act=g.L), "tobacco_c3_1bar_test_28K.csv", ch4_binspec)
-# 
-train_data <- read_csv("tobacco_c3_1bar_train_28K.csv")
-test_data <- read_csv("tobacco_c3_1bar_test_28K.csv")
 
 # ORIGINAL MODEL
- model1 <- randomForest(x = train_data %>% select(-y_act, -`Host-Guest`, -`Guest-Guest`, -Heat_of_Ads, -Qst_low_loading), y = train_data$y_act, ntree = 500)
-# DESELECT `Host-Guest` ,HEAT OF ADS and Low_qst
- model2 <- randomForest(x = train_data %>% select(-y_act, -`Host-Guest`, -Heat_of_Ads,-Qst_low_loading), y = train_data$y_act, ntree = 500)
-# DESELECT `Guest-Guest` and HEAT OF ADS and Low_qst
- model3 <- randomForest(x = train_data %>% select(-y_act, -`Guest-Guest`, -Heat_of_Ads,-Qst_low_loading), y = train_data$y_act, ntree = 500)
-# DESELECT `Host-Guest` and `Guest-Guest` and low_qst
- model4 <- randomForest(x = train_data %>% select(-y_act, -`Guest-Guest`, -`Host-Guest`,-Qst_low_loading), y = train_data$y_act, ntree = 500)
-# DESELECT JUST HEAT OF ADSORPTION and low_qst
- model5 <- randomForest(x = train_data %>% select(-y_act, -Heat_of_Ads, -Qst_low_loading), y = train_data$y_act, ntree = 500)
-# ADD LOW LOADING QST INTO THE MODEL(ALONE)
- model6 <- randomForest(x = train_data %>% select(-y_act, -`Host-Guest`, -`Guest-Guest`, -Heat_of_Ads), y = train_data$y_act, ntree = 500)
-# COUPLE GUEST-GUEST with Low Loading Qst
- model7 <- randomForest(x = train_data %>% select(-y_act, -`Host-Guest`, -Heat_of_Ads), y = train_data$y_act, ntree = 500)
- 
+ rf_model <- randomForest(x = train_data %>% select(-y_act), y = train_data$y_act, ntree = 500)
  # gg <- qplot(x = model$predicted, y = model$y) + geom_abline(slope = 1, intercept = 0)
 
 # # plot error automatically if model is a randomforest
 # #plot(model)
 # # making predictions/ testing your fit, tell what variables are important
-# #varImpPlot(model)
+ #varplot <- varImpPlot(rf_model)
+ #save_plot(paste("Results/", paste0(molecule_name, "_Variable_Importance_Plot.png"), sep = ""),varplot, base_width = 10, base_height = 8, dpi = 600)
 # 
-tested1 <- predict(model1, test_data)
-tested2 <- predict(model2, test_data)
-tested3 <- predict(model3, test_data)
-tested4 <- predict(model4, test_data)
-tested5 <- predict(model5, test_data)
-tested6 <- predict(model6, test_data)
-tested7 <- predict(model7, test_data)
+ tested1 <- predict(rf_model, test_data)
 
 # gg_test <- qplot(x = test_data$y_act, y = tested) + geom_abline(slope = 1, intercept = 0)
-# 
-tested_rmse1 <- postResample(pred = tested1, obs = test_data$y_act) # original model
-tested_rmse2 <- postResample(pred = tested2, obs = test_data$y_act) # just guest-guest
-tested_rmse3 <- postResample(pred = tested3, obs = test_data$y_act) # just host-guest
-tested_rmse4 <- postResample(pred = tested4, obs = test_data$y_act) # just heat of ads
-tested_rmse5 <- postResample(pred = tested5, obs = test_data$y_act) # gg and hg
-tested_rmse6 <- postResample(pred = tested6, obs = test_data$y_act) # just qst at low loading(1000pa)
-tested_rmse7 <- postResample(pred = tested7, obs = test_data$y_act) # gg and qst low loading
-
-# # #read the names with topology
+ rf_tested_rmse <- postResample(pred = tested1, obs = test_data$y_act) # original model
+ gg_rf_train <- qplot(x = rf_model$y, y = rf_model$predicted) + geom_abline(slope = 1, intercept = 0, linetype = 2) + xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)")
+ save_plot(paste("Results/", paste0(molecule_name, "_random_forest_train.png"), sep = ""),gg_rf_train, base_width = 10, base_height = 8, dpi = 600)
+ gg_rf_test <- qplot(x = test_data$y_act, y = tested1) + geom_abline(slope = 1, intercept = 0, linetype = 2)+ xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)")
+ save_plot(paste("Results/", paste0(molecule_name, "_random_forest_test.png"), sep = ""),gg_rf_test, base_width = 10, base_height = 8, dpi = 600)
+ # # #read the names with topology
 topologies <- read.table("All_data/fullnames.txt") # first column is ID, second column is topology
 x <- vector(mode="character", length = dim(p_ch4_vol$pred_df)[1])
 n1is_sym3 <- vector(mode="character", length = dim(p_ch4_vol$pred_df)[1])
