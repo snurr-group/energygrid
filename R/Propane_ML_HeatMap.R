bdcc21301fbfe64f3f2764cdf07b7291af9423a6
zhaoli2023 <43864970+Zhaoli2042@users.noreply.github.com>
2019-07-26 18:26:17 -0500

    Using Heatmap instead of histogram to fit It performs better than histogram alone. Performs a little better than histogram + topology for hexane fitting. Still outliers exist. Also fixed bug in previous version.

--- R/Propane_ML_HeatMap.R
+++ R/Propane_ML_HeatMap.R
@@ -1,48 +1,51 @@
+rm(list=ls())
+# for checking package availability, if not, install it
+source("R/package_verification.R")
+
 source("R/plot_hists.R")
 source("R/regression_and_features.R")
 source("R/get_energy_stats.R")
 source("R/plot_diagnostics.R")
-library(plotly)
-library(randomForest)
-source("R/read_tobacco_new_propane.R")
-source("R/tobacco_data_for_zhao.R")
+source("R/read_gcmc_data.R")
+source("R/read_tobacco_data.R")
 source("R/save_train_test_data.R")
 source("R/refined_bins_calc.R")
 # in command prompt: Rscript --vanilla R\save_h2_hists.R whatever.rds Energies\ use_ch4
-
-#ch4_binspec <- c(from=-26 , to=20, step=0.5, width=0.5)
-
-hmof_h2_grid <- read_rds("All_data/Grids_Propane_298K_1A_GenericMOFs.Rds")
-
+# define the input files
+grid_file = "All_data/CH3_probe_1A_UFF.rds"
+gcmc_file = "All_data/Hexane_.1bar_new.txt"
+if (!dir.exists("Results")){
+  dir.create("Results")
+}
+hmof_h2_grid <- read_rds(grid_file)
+gcmc_data <- read_data(gcmc_file)
+# extract the molecule name for picture naming
+molecule_name <- sub(".*\\/", "", gcmc_file) %>% sub("\\_.*", "", .)
 # here we can have two options, feed uniform or non-uniform bins
 #binbounds <- bounds_from_params(ch4_binspec)
 gcmc_data <- mutate(gcmc_data, id=ID)
 new_grid <- hmof_h2_grid[hmof_h2_grid[, "id"] %in% gcmc_data$id,] # select out the grids with id same as gcmc_data id
-binbounds <- automatic_bins(new_grid,1000000) # for non-uniform bins
+binbounds <- automatic_bins(hmof_h2_grid) # for non-uniform bins
+# further lump all repulsive bins together
+binbounds <- as.data.frame(binbounds)
+binbounds <- binbounds %>% filter(upper <= 0.0)
 # get the names of files from that path
 namess <- list.files(path="Energies/") %>% str_remove(., ".grid")
 gcmc_data <- gcmc_data %>% filter(ID %in% namess)
-source("R/get_heat_map.R")
-# get the names from gcmc_data$ID
+#source("R/get_heat_map.R")
+asdasd <- readRDS("All_data/heat_map_UFF_grads.rds")
+a <- asdasd
+gcmc_data <- gcmc_data %>% filter(ID %in% a$ID)
+a <- a %>% filter(ID %in% gcmc_data$ID)
+aa <-  a %>% select(-ID)
+# sometimes gcmc_data are available but grid is not
 
-# count = 0
-# for (MOF_ID in gcmc_data$ID){
-#   abvtf <- heat_maps(MOF_ID, binbounds)
-#   count = count + 1
-#   if (count == 1) {
-#     abvtfs <- abvtf
-#   } else{
-#     abvtfs <- rbind(abvtfs, abvtf) 
-#   }
-# }
-#saveRDS(abvtfs, "heat_map.rds")
-abvtfs <- readRDS("heat_map.rds")
-abvtfs$rsums <- rowSums(abvtfs %>% select(-ID))
-a <- abvtfs %>% select(-ID, -rsums)
-a <- a/abvtfs$rsums
-a$ID <- abvtfs$ID
-
-heat_maps <- cbind(a, gcmc_data %>% select(ID, Molec_cm3overcm3)) # may generate a duplicated colname
+# knock out columns and normalize
+dd1 <- aa[,colSums(aa[, 1:ncol(aa)]) > 0.3]
+dd1 <- dd1/rowSums(dd1)
+dd1$ID <- a$ID
+dd1 <- dd1[order(match(dd1$ID, gcmc_data$ID)),] # without this line, it won't be right
+heat_maps <- cbind(dd1, gcmc_data %>% select(ID, Molec_cm3overcm3)) # may generate a duplicated colname
 # remove the duplicated column name
 heat_maps <-heat_maps[, !duplicated(colnames(heat_maps))]
 
@@ -54,7 +57,7 @@ trained_model <- training_data %>% select(-ID, -Molec_cm3overcm3) %>% fit_glmnet
 
 tested <- pred_glmnet(trained_model, testing_data %>% select(-ID, -Molec_cm3overcm3))
 postResample(tested, testing_data$Molec_cm3overcm3)
-qplot(x = testing_data$Molec_cm3overcm3, y = tested_rf) + geom_abline(slope = 1, intercept = 0)
+qplot(x = testing_data$Molec_cm3overcm3, y = tested) + geom_abline(slope = 1, intercept = 0) + scale_x_continuous(limits=c(0, 200))+ scale_y_continuous(limits=c(0, 200))
 
 trained_rf <- randomForest(x = training_data %>% select(-ID, -Molec_cm3overcm3), y = training_data$Molec_cm3overcm3, ntree = 500)
 tested_rf <- predict(trained_rf, testing_data %>% select(-ID, Molec_cm3overcm3))
