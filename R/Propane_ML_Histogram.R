fc15b6e75cc05bd4a808c7d6bb77dc92bcae7082
zhaoli2023 <43864970+Zhaoli2042@users.noreply.github.com>
2019-08-01 14:22:26 -0500

    Modified where are the plots are saved. Now it is more automatic!

--- R/Propane_ML_Histogram.R
+++ R/Propane_ML_Histogram.R
@@ -13,14 +13,23 @@ source("R/refined_bins_calc.R")
 # in command prompt: Rscript --vanilla R\save_h2_hists.R whatever.rds Energies\ use_ch4
  # define the input files
  grid_file = "All_data/CH3_probe_1A_UFF.rds"
- gcmc_file = "All_data/Hexane_.1bar_new.txt"
+ gcmc_file = "All_data/propane_10bar_uff.txt"
  if (!dir.exists("Results")){
    dir.create("Results")
  }
  hmof_h2_grid <- read_rds(grid_file)
  gcmc_data <- read_data(gcmc_file)
- # extract the molecule name for picture naming
+ # extract the molecule name, pressure and temperature for picture naming
  molecule_name <- sub(".*\\/", "", gcmc_file) %>% sub("\\_.*", "", .)
+ temperature <- gcmc_data$Temp[1]
+ pressure <- gcmc_data$Pres[1]
+ string_to_paste <- paste(molecule_name, temperature, pressure, sep = "_")
+ # create a directory for that condition and molecule
+ save_path <- paste0(string_to_paste, "/", sep = "")
+ save_path <- paste0("Results/", save_path)
+ if (!dir.exists(save_path)){
+   dir.create(save_path)
+ }
  # here we can have two options, feed uniform or non-uniform bins
  #binbounds <- bounds_from_params(ch4_binspec)
  gcmc_data <- mutate(gcmc_data, id=ID)
@@ -45,8 +54,8 @@ p_ch4_vol <- gcmc_data %>%
 
 gg_train <- p_ch4_vol$plots$parity_training%>% rescale_ch4_parity() + theme(axis.title.y = element_text(hjust=0.5)) + xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)")
 gg_test <- p_ch4_vol$plots$parity_testing%>% rescale_ch4_parity() + theme(axis.title.y = element_text(hjust=0.5)) + xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)")
-save_plot(paste("Results/", paste0(molecule_name, "_train.png"), sep = ""),gg_train, base_width = 10, base_height = 8, dpi = 600)
-save_plot(paste("Results/", paste0(molecule_name, "_test.png"), sep = ""),gg_test, base_width = 10, base_height = 8, dpi = 600)
+save_plot(paste(save_path, paste0(string_to_paste, "_LASSO_train.png"), sep = ""),gg_train, base_width = 10, base_height = 8, dpi = 600)
+save_plot(paste(save_path, paste0(string_to_paste, "_LASSO_test.png"), sep = ""),gg_test, base_width = 10, base_height = 8, dpi = 600)
 # export data for later testing other models, one can also export data to csv files
  train_data <- export_data(p_ch4_sets$training, rename(gcmc_data %>% mutate(g.L = Molec_cm3overcm3), y_act=g.L), ch4_binspec)
  test_data <- export_data(p_ch4_sets$testing, rename(gcmc_data %>% mutate(g.L = Molec_cm3overcm3), y_act=g.L), ch4_binspec)
@@ -63,16 +72,16 @@ save_plot(paste("Results/", paste0(molecule_name, "_test.png"), sep = ""),gg_tes
 # #plot(model)
 # # making predictions/ testing your fit, tell what variables are important
  #varplot <- varImpPlot(rf_model)
- #save_plot(paste("Results/", paste0(molecule_name, "_Variable_Importance_Plot.png"), sep = ""),varplot, base_width = 10, base_height = 8, dpi = 600)
+ #save_plot(paste("Results/", paste0(string_to_paste, "_Variable_Importance_Plot.png"), sep = ""),varplot, base_width = 10, base_height = 8, dpi = 600)
 # 
  tested1 <- predict(rf_model, test_data)
 
 # gg_test <- qplot(x = test_data$y_act, y = tested) + geom_abline(slope = 1, intercept = 0)
  rf_tested_rmse <- postResample(pred = tested1, obs = test_data$y_act) # original model
  gg_rf_train <- qplot(x = rf_model$y, y = rf_model$predicted) %>% rescale_ch4_parity() + geom_abline(slope = 1, intercept = 0, linetype = 2) + xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)")+ geom_point(color='orange')
- save_plot(paste("Results/", paste0(molecule_name, "_random_forest_train.png"), sep = ""),gg_rf_train, base_width = 10, base_height = 8, dpi = 600)
+ save_plot(paste(save_path, paste0(string_to_paste, "_random_forest_train.png"), sep = ""),gg_rf_train, base_width = 10, base_height = 8, dpi = 600)
  gg_rf_test <- qplot(x = test_data$y_act, y = tested1) %>% rescale_ch4_parity() + geom_abline(slope = 1, intercept = 0, linetype = 2)+ xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)")+ geom_point(color='lightblue')
- save_plot(paste("Results/", paste0(molecule_name, "_random_forest_test.png"), sep = ""),gg_rf_test, base_width = 10, base_height = 8, dpi = 600)
+ save_plot(paste(save_path, paste0(string_to_paste, "_random_forest_test.png"), sep = ""),gg_rf_test, base_width = 10, base_height = 8, dpi = 600)
 
 # Part below are related to topology, see Paper by Yamil and Diego
 # Read the names with topology
@@ -106,13 +115,13 @@ training_rows <- sample(length(all_data_with_R$MOF.ID), DATA_SPLIT)
 training_data_with_R <- all_data_with_R[training_rows, ]
 testing_data_with_R <- all_data_with_R[-training_rows, ]
 
-rf_model_Rscore <- randomForest(x = training_data_with_R %>% select(-y_act, -MOF.ID), y = training_data_with_R$y_act, ntree = 500)
+rf_model_Rscore <- randomForest(x = training_data_with_R %>% select(-y_act, -MOF.ID), y = training_data_with_R$y_act, ntree = 700)
 tested_Rscore <- predict(rf_model_Rscore, testing_data_with_R)
 rf_tested_Rscore_rmse <- postResample(pred = tested_Rscore, obs = testing_data_with_R$y_act) 
-gg_rf_train_Rscore <- qplot(x = rf_model_Rscore$y, y = rf_model_Rscore$predicted) %>% rescale_ch4_parity() + geom_abline(slope = 1, intercept = 0, linetype = 2) + xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)") + geom_point(color='orange')
-save_plot(paste("Results/", paste0(molecule_name, "_random_forest_train_Rscore.png"), sep = ""),gg_rf_train_Rscore, base_width = 10, base_height = 8, dpi = 600)
+gg_rf_train_Rscore <- qplot(x = rf_model_Rscore$y, y = rf_model_Rscore$predicted, label = training_data_with_R$MOF.ID) %>% rescale_ch4_parity() + geom_abline(slope = 1, intercept = 0, linetype = 2) + xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)") + geom_point(color='orange')
+save_plot(paste(save_path, paste0(string_to_paste, "_random_forest_train_Rscore.png"), sep = ""),gg_rf_train_Rscore, base_width = 10, base_height = 8, dpi = 600)
 gg_rf_test_Rscore <- qplot(x = testing_data_with_R$y_act, y = tested_Rscore) %>% rescale_ch4_parity() + geom_abline(slope = 1, intercept = 0, linetype = 2)+ xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)") + geom_point(color='lightblue')
-save_plot(paste("Results/", paste0(molecule_name, "_random_forest_test_Rscore.png"), sep = ""),gg_rf_test_Rscore, base_width = 10, base_height = 8, dpi = 600)
+save_plot(paste(save_path, paste0(string_to_paste, "_random_forest_test_Rscore.png"), sep = ""),gg_rf_test_Rscore, base_width = 10, base_height = 8, dpi = 600)
 
 # learning using Topology data
 topo_train_data<- merge(train_data_with_id, structural_data, by ="MOF.ID")
@@ -123,6 +132,6 @@ rf_model_topo <- randomForest(x = topo_train_data %>% select(-y_act, -MOF.ID), y
 tested_topo <- predict(rf_model_topo, topo_test_data)
 rf_tested_topo_rmse <- postResample(pred = tested_topo, obs = topo_test_data$y_act) 
 gg_rf_train_topo <- qplot(x = rf_model_topo$y, y = rf_model_topo$predicted) %>% rescale_ch4_parity() + geom_abline(slope = 1, intercept = 0, linetype = 2) + xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)") + geom_point(color='orange')
-save_plot(paste("Results/", paste0(molecule_name, "_random_forest_train_topology.png"), sep = ""),gg_rf_train_topo, base_width = 10, base_height = 8, dpi = 600)
+save_plot(paste(save_path, paste0(string_to_paste, "_random_forest_train_topology.png"), sep = ""),gg_rf_train_topo, base_width = 10, base_height = 8, dpi = 600)
 gg_rf_test_topo <- qplot(x = topo_test_data$y_act, y = tested_topo) %>% rescale_ch4_parity() + geom_abline(slope = 1, intercept = 0, linetype = 2)+ xlab("GCMC capacity (cm\u00B3/cm\u00B3)") + ylab("Predicted capacity (cm\u00B3/cm\u00B3)") + geom_point(color='lightblue')
-save_plot(paste("Results/", paste0(molecule_name, "_random_forest_test_topology.png"), sep = ""),gg_rf_test_topo, base_width = 10, base_height = 8, dpi = 600)
+save_plot(paste(save_path, paste0(string_to_paste, "_random_forest_test_topology.png"), sep = ""),gg_rf_test_topo, base_width = 10, base_height = 8, dpi = 600)
