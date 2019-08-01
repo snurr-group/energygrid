6eb70c142ec5d97ba13fd1137faba7428f88b876
zhaoli2023 <43864970+Zhaoli2042@users.noreply.github.com>
2019-08-01 14:24:39 -0500

    Modified where the plots are saved. It takes the molecule name, the temperature and pressure, generate a folder and save plots to that folder. Neat!

--- R/get_heat_map.R
+++ R/get_heat_map.R
@@ -6,9 +6,10 @@ library(manipulate)
 library(slam) # for calculating row norm
 library(gplots)
 source("R/hist2d_breaks.R")
+
 heat_maps <- function(MOF_ID, binbounds) {
-  energy <- read_table(paste0("Energies/", MOF_ID, ".grid"), col_names = "V1", col_types = "d")$V1  # faster than read_tsv for similar purposes
-  gradients <- read_table2(paste0("Gradients/", MOF_ID, ".grid"), col_names = FALSE)
+  energy <- read_table(paste0("GenericMOF_Energies/", MOF_ID, ".grid"), col_names = "V1", col_types = "d")$V1  # faster than read_tsv for similar purposes
+  gradients <- read_table2(paste0("GenericMOF_Gradients/", MOF_ID, ".grid"), col_names = FALSE)
   
   eng_grad <- gradients
   eng_grad$energy <- energy
@@ -22,16 +23,21 @@ heat_maps <- function(MOF_ID, binbounds) {
   # ggplot(latest_grad, aes(x = energy, y = grad_size)) + stat_bin_2d()
   a <- append(binbounds$upper, -Inf, 0)
   a <- append(a, Inf, length(a))
-  energy_breaks <- binbounds$upper
+  energy_breaks <- a
   
-  grad_breaks <- c(0, 7, 350, 11000, Inf) # taken from the quantile values of 1.grid
+  #grad_breaks <- c(0, 7, 350, 11000, Inf) # taken from the quantile values of 1.grid from tobacco
+  #grad_breaks <- c(0, 1.5, 2, 3, 50, 350, Inf) # finer values
   
+  grad_breaks <- c(0, 1, 1.5, 2, 2.5, 3, 4, 6, 8, 10, 20, 30, 40, 50, 350, Inf)
   abc <- hist2d_break(x = new_grad$energy, y = new_grad$grad_size, energy_breaks, grad_breaks, show = FALSE)
   # convert matrix to a 1D dataframe with MOFID
+  abc$counts <- abc$counts/sum(abc$counts)
+  saveRDS(abc$counts, paste0("HeatMaps/", MOF_ID, ".rds"))
   ab <- abc$counts #get the counts from the 2d histogram
-  colnames(ab) <- c("low", "midlow", "midhigh", "high")
+  colnames(ab) <- seq(from = 1, to = 15, by = 1)
   rownames(ab) <- seq(from = 1, to = nrow(ab), by = 1)
   abv <- as.vector(ab)
+  
   names(abv) <- seq(from = 1, to = length(abv), by = 1)
   abvt <- t(abv)
   abvtf <- as.data.frame(abvt)
@@ -39,3 +45,18 @@ heat_maps <- function(MOF_ID, binbounds) {
   abvtf$ID <- MOF_ID
   abvtf
 }
+
+namess <- list.files(path="Energies/") %>% str_remove(., ".grid")
+
+count = 0
+for (MOF_ID in namess){
+  abvtf <- heat_maps(MOF_ID, binbounds)
+  count = count + 1
+  if (count == 1) {
+    abvtfs <- abvtf
+  } else{
+    abvtfs <- rbind(abvtfs, abvtf)
+  }
+}
+saveRDS(abvtfs, "heat_map_more_grads.rds")
+
