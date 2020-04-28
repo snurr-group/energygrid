1b92b3d7f6bfad9394a71f2ee14f73a7c42ddad0
zhaoli2042 <zhaoli2023@u.northwestern.edu>
2020-04-28 09:44:37 -0500

    change how the lines are arranged

--- R/package_verification.R
+++ R/package_verification.R
@@ -1,4 +1,14 @@
-packages = c("plotly","randomForest","cowplot", "ggplot2", "dplyr", "stringr", "magrittr", "readr", "tidyr", "openxlsx", "tidyverse", "R.utils", "manipulate", "slam", "gplots", "purrr", "glmnet", "caret", "testthat", "readxl","grid")
+packages = c("plotly","randomForest",
+             "cowplot", "ggplot2", 
+             "dplyr", "stringr", 
+             "magrittr", "readr", 
+             "tidyr", "openxlsx", 
+             "tidyverse", "R.utils", 
+             "manipulate", "slam", 
+             "gplots", "purrr", 
+             "glmnet", "caret", 
+             "testthat", "readxl", 
+             "grid")
 package.check <- lapply(packages, FUN = function(x) {
     if (!require(x, character.only = TRUE)) {
         install.packages(x, dependencies = TRUE)
