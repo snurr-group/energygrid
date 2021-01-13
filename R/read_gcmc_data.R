209388de027fd9f96c8759555e2a2fbc7353623c
zhaoli2042 <zhaoli2023@u.northwestern.edu>
2021-01-13 01:05:42 -0600

    change unit name, make it more readable

--- R/read_gcmc_data.R
+++ R/read_gcmc_data.R
@@ -30,10 +30,10 @@ mc_0_ids <- read_table("All_data/IDs_with_mc_0.txt")
 #mc_0_ids <- as.character(mc_0_ids)
 # for tobacco mofs, we exclude the dubious mc_0 node, all mofs with that ID will be filtered
 
-if (unit_of_ads == "Molec_cm3overcm3"){
+if (unit_of_ads == "cm3overcm3"){
   gcmc_data <- gcmc_data %>% select(ID, Temp, Pres, Molec_cm3overcm3)
   # rename the column using a general name
-  colnames(gcmc_data)[colnames(gcmc_data) == "Molec_cm3overcm3"] <- "Uptake"
+  colnames(gcmc_data)[colnames(gcmc_data) == "cm3overcm3"] <- "Uptake"
   unit_for_plot <<- "cm\u00B3/cm\u00B3"
   
 } else if (unit_of_ads == "Mill_gram"){
@@ -81,15 +81,5 @@ if (just2k)
 
 gcmc_data
 }
-# low_loading <- read_table2("All_data/propane_1000pa_140K.txt")
-# low_loading_heat  <- low_loading %>% select(ID, Heat_of_Ads)
-# names(low_loading_heat)[2] <- "Qst_low_loading"
-# low_loading_heat <- na.omit(low_loading_heat)
-# hggg <- read_table2("All_data/HGGG_data_per_molec_1bar_28K.txt")
-# #hggg <- read_table2("HGGG_data_1bar_28K_float.txt")
-# hggg <- na.omit(hggg)
-# #write.xlsx(gcmc_data, "propane_1000pa.xlsx")
-# names(hggg)[1] <- "ID"
-# gcmc_data <- merge(gcmc_data, hggg, by="ID")
-# gcmc_data <- merge(gcmc_data, low_loading_heat, by="ID")
+
 
